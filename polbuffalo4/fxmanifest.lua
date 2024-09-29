191cb5625e3d186606b5a2f0c0fca1c4f740b8a7
Nicolas <56158436+foulek57@users.noreply.github.com>
2024-09-29 23:36:58 +0200

    Add files via upload

---
+++ polbuffalo4/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+client_script {
+	'car_names.lua'
+}
+
+files {
+		'vehiclelayouts_police.meta',
+		'handling.meta',
+	    'vehicles.meta',
+		'carcols.meta',
+	    'carvariations.meta',
+		"audioconfig/*.dat151.rel",
+}
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts_police.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/polbuffalo4_game.dat'
